3ef3608f6014da3293136538e32cc5a8cd74d589
Azis Wh <aziswh7@gmail.com>
2026-05-09 21:34:25 +0700

    delete close texr in pengiriman line 460

--- resources/views/admin/pengiriman/index.blade.php
+++ resources/views/admin/pengiriman/index.blade.php
@@ -457,7 +457,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Foto Surat Jalan - Pengiriman #<span id="fotoPengirimanId"></span></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">Close</button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div id="fotoGalleryContainer">
